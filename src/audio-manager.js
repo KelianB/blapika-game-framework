6301225d987ee52d430b60c69077ffff4f6778c8
Kelian Baert <kelian@baert.me>
2016-08-20 19:07:47 +0200

    Added documentation

--- src/audio-manager.js
+++ src/audio-manager.js
@@ -11,21 +11,33 @@ Engine.prototype.AudioManager = new function() {
     this.playing = [];
     this.muted = false;
 
+    // Adds a function to the Audio prototype to fade the volume to a given value in a given duration.
     Audio.prototype.fadeTo = function(targetVolume, duration) {
         $(this).animate({volume: targetVolume}, duration);
     };
 
+    /** Sets the volume of all sounds (will be applied to the sounds that are played later).
+     * @param {Number} volume - The volume value, between 0 and 1.
+     */
     this.setGlobalVolume = function(volume) {
         this.globalVolume = volume;
-        for(var i = 0; i < this.playing.length; i++) {
+        for(var i = 0; i < this.playing.length; i++)
             this.playing[0].volume = this.globalVolume;
-        }
     };
 
+    /** Plays audio after cloning the audio element, allowing it to be played multiple times simultaneously.
+     * @param {Audio} audio - The audio element to play.
+     * @param {Object} [fadeIn] - An object describing how to fade the audio (properties: start, end, duration).
+     */
     this.playRepeatable = function(audio, fadeIn) {
         this.play(audio.cloneNode(), false, fadeIn);
     };
 
+    /** Plays audio.
+     * @param {Audio} audio - The audio element to play.
+     * @param {Boolean} [reset=false] - Whether or not to reset the currentTime of the Audio before playing it.
+     * @param {Object} [fadeIn] - An object describing how to fade the audio (properties: start, end, duration).
+     */
     this.play = function(audio, reset, fadeIn) {
         if(reset)
             audio.currentTime = 0;
@@ -46,7 +58,10 @@ Engine.prototype.AudioManager = new function() {
         audio.play();
     };
 
-    // If count is undefined => loop indefinitively
+    /** Plays audio repeatedly.
+     * @param {Audio} audio - The audio element to play.
+     * @param {Integer} [count=infinite] - How many times the audio should be played.
+     */
     this.loop = function(audio, count) {
         if(count == undefined)
             count = -1;
@@ -61,6 +76,7 @@ Engine.prototype.AudioManager = new function() {
         }
     };
 
+    /** Toggles mute for all audio. */
     this.toggleMute = function() {
         this.muted = !this.muted;
 
@@ -68,6 +84,9 @@ Engine.prototype.AudioManager = new function() {
             this.playing[i].muted = this.muted;
     };
 
+    /** Stops all audio currently playing.
+     * @param {Number} [fadeOutDuration] - The duration of the fade-out effect.
+     */
     this.stopAll = function(fadeOutDuration) {
         for(var i = 0; i < this.playing.length; i++) {
             var audio = this.playing[i];
@@ -83,6 +102,4 @@ Engine.prototype.AudioManager = new function() {
             }
         }
     };
-
-
 };
