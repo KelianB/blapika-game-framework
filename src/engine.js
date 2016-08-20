1e7a12be9be1ad606f68261d5ad11926612bdd6f
Kelian Baert <kelian@baert.me>
2016-08-20 18:58:21 +0200

    Added documentation in engine.js

--- src/engine.js
+++ src/engine.js
@@ -1,23 +1,27 @@
 /** Initiates the engine.
- * @class 
+ * @class
  * @classdesc Manages modules.
  */
 function Engine() {
     var self = this;
     var allModules = ["core", "animation", "audio-manager", "resource-manager", "debug"];
-    
+
     /** The current version of the engine. */
     this.version = "0.1";
-    
+
     /** The root of the module files. */
-    this.root = "/"; 
-    
-    /** Stores loaded modules */
+    this.root = "/";
+
+    /** Stores loaded modules. */
     this.loadedModules = [];
-    
+
+    /** Loads the given modules.
+     * @param {Array} modules - The name of the modules to load.
+     * @param {Function} [callback] - A function that will be called when the modules are done loading.
+     */
     this.loadModules = function(modules, callback) {
         var toLoad = modules.length;
-        
+
         for(var i = 0; i < modules.length; i++) {
             $.getScript(self.root + modules[i] + ".js", function() {
                 self.loadedModules.push(modules[i]);
@@ -25,16 +29,22 @@ function Engine() {
                     callback();
             })
         }
-    }; 
-    
+    };
+
+    /** Loads every single module available in this engine.
+     * @param {Function} [callback] - A function that will be called when the modules are done loading.
+     */
     this.loadAllModules = function(callback) {
-        this.loadModules(allModules, callback);  
-    }; 
-    
+        this.loadModules(allModules, callback);
+    };
+
+    /** Checks if a given module is loaded.
+     * @param {String} moduleName - The name of the module to check for.
+     */
     this.isModuleLoaded = function(moduleName) {
         return this.loadedModules.indexOf(moduleName) != -1;
     };
-
 };
 
+// Initialize the engine
 var engine = new Engine();
